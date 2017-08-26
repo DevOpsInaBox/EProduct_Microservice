6977851fdf3c48b9a1fd5697d44b6f37a5d7046a
Devops <devopsInabox@newtglobal.com>
2017-08-27 01:14:58 +0530

    Update DataWithMicroservicesJpaApplication.java

--- src/main/java/com/newt/DataWithMicroservicesJpaApplication.java
+++ src/main/java/com/newt/DataWithMicroservicesJpaApplication.java
@@ -15,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration
 public class DataWithMicroservicesJpaApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DataWithMicroservicesJpaApplication.class, args);
 	}
